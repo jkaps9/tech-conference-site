6ff10eb2433dd5c172418d4602e5c28bd2608ffd
Josh <15219491+jkaps9@users.noreply.github.com>
2026-05-12 22:37:40 -0400

    remove blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,18 +5,14 @@ import { defineCollection } from "astro:content";
 // Import Zod
 import { z } from "astro/zod";
 // Define a `loader` and `schema` for each collection
-const blogsCollection = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/blog" }),
+const tracksCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/tracks" }),
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
+      name: z.string(),
       description: z.string(),
-      author: z.string(),
-      date: z.date(),
-      image: image().optional(),
-      imageAlt: z.string().optional(),
-      isFeatured: z.boolean().optional().default(false),
+      color: z.string(),
     }),
 });
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog: blogsCollection };
+export const collections = { track: tracksCollection };
